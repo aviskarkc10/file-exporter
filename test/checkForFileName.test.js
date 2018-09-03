0a804eb11f26f26101ada14bff3eb97b9abc7b26
Aviskar KC <aviskarkc10@gmail.com>
2018-09-03 17:33:21 +0545

    Add additional test cases for checking if filename is string

--- test/checkForFileName.test.js
+++ test/checkForFileName.test.js
@@ -5,7 +5,25 @@ describe('checking file name for string', () => {
   it('should return undefined as filename exists', () => {
     expect(checkFileNameForString('flags.csv')).to.equal(undefined);
   })
+  it('should return undefined as filename exists', () => {
+    expect(checkFileNameForString('flags')).to.equal(undefined);
+  })
   it('should throw an error for non-existant filename', () => {
-    expect(() => checkFileNameForString()).to.throw('The filename should be a string.');   
+    expect(() => checkFileNameForString()).to.throw('The filename should be a string.');
+  })
+  it('should throw an error for non-existant filename', () => {
+    expect(() => checkFileNameForString([1, 2, 3])).to.throw('The filename should be a string.');
+  })
+  it('should throw an error for non-existant filename', () => {
+    expect(() => checkFileNameForString(10000)).to.throw('The filename should be a string.');
+  })
+  it('should throw an error for non-existant filename', () => {
+    expect(() => checkFileNameForString({ name: 'Mocha Test' })).to.throw('The filename should be a string.');
+  })
+  it('should throw an error for non-existant filename', () => {
+    expect(() => checkFileNameForString(() => 5)).to.throw('The filename should be a string.');
+  })
+  it('should throw an error for non-existant filename', () => {
+    expect(() => checkFileNameForString(() => '5')).to.throw('The filename should be a string.');
   })
 })
