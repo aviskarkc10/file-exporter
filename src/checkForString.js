85f9b5e4b608673d05c484f225df0acecc4f3ec1
Aviskar KC <aviskarkc10@gmail.com>
2018-09-03 17:28:18 +0545

    Fix grammtical mistake on data and filename should be string error messages

--- src/checkForString.js
+++ src/checkForString.js
@@ -1,11 +1,11 @@
 export function checkDataForString(data) {
   if (typeof data !== 'string') {
-   throw new Error('The data should a string.');
+   throw new Error('The data should be a string.');
   }
 }
 
 export function checkFileNameForString(fileName) {
   if (typeof fileName !== 'string') {
-   throw new Error('The filename should a string.');
+   throw new Error('The filename should be a string.');
   }
 }
