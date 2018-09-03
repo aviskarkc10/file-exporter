7a389c63c311c97ad70f3b538a0d42830149ccde
Aviskar KC <aviskarkc10@gmail.com>
2018-09-03 17:52:07 +0545

    Stupid copy paste

--- test/checkForFileName.test.js
+++ test/checkForFileName.test.js
@@ -11,19 +11,19 @@ describe('checking file name for string', () => {
   it('should throw an error for non-existant filename', () => {
     expect(() => checkFileNameForString()).to.throw('The filename should be a string.');
   })
-  it('should throw an error for non-existant filename', () => {
+  it('should throw an error for array as filename', () => {
     expect(() => checkFileNameForString([1, 2, 3])).to.throw('The filename should be a string.');
   })
-  it('should throw an error for non-existant filename', () => {
+  it('should throw an error for number as filename', () => {
     expect(() => checkFileNameForString(10000)).to.throw('The filename should be a string.');
   })
-  it('should throw an error for non-existant filename', () => {
+  it('should throw an error for object as filename', () => {
     expect(() => checkFileNameForString({ name: 'Mocha Test' })).to.throw('The filename should be a string.');
   })
-  it('should throw an error for non-existant filename', () => {
+  it('should throw an error for function as filename', () => {
     expect(() => checkFileNameForString(() => 5)).to.throw('The filename should be a string.');
   })
-  it('should throw an error for non-existant filename', () => {
+  it('should throw an error for function as filename', () => {
     expect(() => checkFileNameForString(() => '5')).to.throw('The filename should be a string.');
   })
 })
