c7fe844546481c081ecc507c5d95cc0fca5da451
Aviskar KC <aviskarkc10@gmail.com>
2018-09-03 17:51:02 +0545

    Reverse the order of validations for filenames

--- src/checkForFileName.js
+++ src/checkForFileName.js
@@ -1,8 +1,8 @@
 import { checkFileNameForString } from "./checkForString";
 
 export function checkForFileName(fileName) {
-  checkFileNameForString(fileName);
   if(!fileName) {
     throw new Error('Filename is required.');
   }
+  checkFileNameForString(fileName);
 }
