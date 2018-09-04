f3e02e2c2d3f701e4ac6b8bb20c0da5fc839684a
Aviskar KC <aviskarkc10@gmail.com>
2018-09-04 10:19:27 +0545

    Use default exports to export the functions

--- src/index.js
+++ src/index.js
@@ -2,3 +2,4 @@ import { downloadPDFFromBase64 } from './exportPDF.js';
 import { downloadCSV, downloadCSVFromBase64 } from './exportCSV';
 
 export { downloadCSV, downloadCSVFromBase64, downloadPDFFromBase64 };
+export default { downloadCSV, downloadCSVFromBase64, downloadPDFFromBase64 };
